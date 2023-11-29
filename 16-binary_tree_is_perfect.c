26f8568a92019281513ecfd4f46a1bce18c5b1e1
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-11-29 01:06:39 -0800

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -6,23 +6,54 @@
  *
  * Return: 1 if the tree is perfect, 0 otherwise
  */
+int get_depth(const binary_tree_t *tree)
+{
+	int depth = 0;
+
+	while (tree)
+	{
+		depth++;
+		tree = tree->left;
+	}
+	return (depth);
+}
+
+
+/**
+  * is_perfect - counts the leaves in a binary tree
+  * @tree: binary tree
+  * @depth: depth of left most side
+  * @level: depth of a chain
+  * Return: 1 if it is perfect 0 if not
+  */
+int is_perfect(const binary_tree_t *tree, int depth, int level)
+{
+	if (!tree)
+		return (1);
+	if (!tree->left && !tree->right)
+	{
+		if (depth == (level + 1))
+			return (1);
+		return (0);
+	}
+	if (!tree->left || !tree->right)
+		return (0);
+	return (is_perfect(tree->right, depth, level + 1) &&
+		is_perfect(tree->left, depth, level + 1));
+}
+
+/**
+  * binary_tree_is_perfect - check if binary tree is full
+  * @tree: binary tree
+  * Return: 1 if full 0 if not
+  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-int left_height, right_height;
+	int depth;
 
-if (tree == NULL)
-return (0);
+	depth = get_depth(tree);
 
-left_height = binary_tree_height(tree->left);
-right_height = binary_tree_height(tree->right);
-
-if (left_height == right_height)
-{
-if (tree->left == NULL && tree->right == NULL)
-return (1);
-return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+	if (!tree)
+		return (0);
+	return (is_perfect(tree, depth, 0));
 }
-
-return (0);
-}
-
