a1b05c7478ceb7c7581845f7ee3a1085b33b6944
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-11-29 00:10:56 -0800

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,16 +12,16 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t lheight, rheight;
+size_t lheight, rheight;
 
-	if (tree == NULL)
-		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
-	lheight = binary_tree_height(tree->left);
-	rheight = binary_tree_height(tree->right);
+if (tree == NULL)
+return (0);
+if (tree->left == NULL && tree->right == NULL)
+return (0);
+lheight = binary_tree_height(tree->left);
+rheight = binary_tree_height(tree->right);
 
-	if (lheight > rheight)
-		return (lheight + 1);
-	return (rheight + 1);
+if (lheight > rheight)
+return (lheight + 1);
+return (rheight + 1);
 }
